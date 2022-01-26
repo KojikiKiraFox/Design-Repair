ff127839fa5342f4a95478d06387fe69433d7237
Fox <90596095+KojikiKiraFox@users.noreply.github.com>
2022-01-26 15:50:02 +0300

    minor improvements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(event){
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]')
-    const closeBtn = document.querySelector('.modal__close')
-    const switchModal = () => {
-        modal.classList.toggle('modal-visible')
+document.addEventListener('DOMContentLoaded', function(event){ // подождали пока загрузится весь сайт
+    const modal = document.querySelector('.modal'); // нашли модальное окно и назвали её modal что бы не искаит каждый раз, const озночает что значение не изменится
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]') // нашли все кнопки что должны вести на модальное окно, так же нашли все кнопки и назвали их modalBtn
+    const closeBtn = document.querySelector('.modal__close') // нашли кнопку закрытия модального окна, назвали closeBtn
+
+    const switchModal = () => { // создали переменную которая означала функцию открытия
+        modal.classList.toggle('modal-visible') // сделали "выключатель" (если модальное окно открыто то оно закроится и на оборот)
     } 
 
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal)
+    modalBtn.forEach(element => { // присвоили функцию кнопки(что открывает окно)
+        element.addEventListener('click', switchModal) // при нажатии на нужные кнопки будет открыто модальное окно
     })
 
-    closeBtn.addEventListener('click', switchModal)
+    closeBtn.addEventListener('click', switchModal) // при нажатии на крест модальное окно закроется
+
+
 });
