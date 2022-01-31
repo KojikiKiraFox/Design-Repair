988914f2cdc2ff185f6e785eadadcf0dda2f7db6
Fox <90596095+KojikiKiraFox@users.noreply.github.com>
2022-01-31 15:40:45 +0300

    feat: section control

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function(event){ // подождали пока загрузится весь сайт
-//     const modal = document.querySelector('.modal'); // нашли модальное окно и назвали её modal что бы не искаит каждый раз, const озночает что значение не изменится
-//     const modalBtn = document.querySelectorAll('[data-toggle=modal]') // нашли все кнопки что должны вести на модальное окно, так же нашли все кнопки и назвали их modalBtn
-//     const closeBtn = document.querySelector('.modal__close') // нашли кнопку закрытия модального окна, назвали closeBtn
-
-//     const switchModal = () => { // создали переменную которая означала функцию открытия
-//         modal.classList.toggle('modal-visible') // сделали "выключатель" (если модальное окно открыто то оно закроится и на оборот)
-//     } 
-
-//     modalBtn.forEach(element => { // присвоили функцию кнопки(что открывает окно)
-//         element.addEventListener('click', switchModal) // при нажатии на нужные кнопки будет открыто модальное окно
-//     })
-
-//     closeBtn.addEventListener('click', switchModal) // при нажатии на крест модальное окно закроется
-
-
-// })
 
 $(document).ready(function () {
     var modal = $('.modal'),
@@ -61,4 +44,7 @@ $(document).ready(function () {
 
     next.css('left', prev.width() + 10 + bullets.width() + 10)
     bullets.css('left', prev.width() + 10)
+
+    new WOW().init();
+    
 });
